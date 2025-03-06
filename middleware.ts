6786109c2b48928bea9fa624fcdb74add9dc14df
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-06 11:25:35 +0530

    fix middlware

--- middleware.ts
+++ middleware.ts
@@ -11,14 +11,16 @@ const protectedRoutes = createRouteMatcher([
 ]);
 
 export default  clerkMiddleware( (auth, req) => {
+  console.log("Midlware invoked for the path: ", req.nextUrl.pathname);
   if(protectedRoutes(req)) auth().protect();
+  return;
 })
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
